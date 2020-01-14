8b504f8a0b78b4aa33857b423087f307171ac0fe
Tomasz Michalak <tmichalak@antmicro.com>
2020-01-14 09:24:22 +0100

    XDC: Add counter attribute annotation test
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

---
+++ xdc-plugin/tests/synth.tcl
@@ -0,0 +1,12 @@
+yosys -import
+plugin -i xdc
+#Import the commands from the plugins to the tcl interpreter
+yosys -import
+# -flatten is used to ensure that the output eblif has only one module.
+# Some of symbiflow expects eblifs with only one module.
+synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp
+
+read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
+
+# Write the design in JSON format.
+write_json $::env(OUT_JSON)
