1d6670165dd74084813b38032cfddb6d33cd9d7a
Tomasz Michalak <tmichalak@antmicro.com>
2020-01-20 11:03:34 +0100

    XDC: Refactor test output comparison script
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xdc-plugin/tests/compare_output_json.py
+++ xdc-plugin/tests/compare_output_json.py
@@ -8,6 +8,7 @@ The return code of this script is used to check if the output is equivalent.
 
 import sys
 import json
+import argparse
 
 parameters = ["IOSTANDARD", "DRIVE", "SLEW", "IN_TERM"]
 
@@ -25,19 +26,27 @@ def read_cells(json_file):
     return cells_parameters
 
 
-def main():
-    if len(sys.argv) < 3:
-        print("Incorrect number of arguments")
-        exit(1)
-    cells1 = read_cells(sys.argv[1])
-    cells2 = read_cells(sys.argv[2])
-    if cells1 == cells2:
-        exit(0)
+def main(args):
+    cells = read_cells(args.json)
+    if args.update:
+        with open(args.golden, 'w') as f:
+            json.dump(cells, f)
     else:
-        print(json.dumps(cells1, indent=4))
-        print("VS")
-        print(json.dumps(cells2, indent=4))
-        exit(1)
+        with open(args.golden) as f:
+            cells_golden = json.load(f)
+            if cells == cells_golden:
+                exit(0)
+            else:
+                print(json.dumps(cells, indent=4))
+                print("VS")
+                print(json.dumps(cells_golden, indent=4))
+                exit(1)
+    f.close()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--json', help = 'JSON to compare', required = True)
+    parser.add_argument('--golden', help = 'Golden JSON file', required = True)
+    parser.add_argument('--update', action = 'store_true', help = 'Update golden reference')
+    args = parser.parse_args()
+    main(args)
