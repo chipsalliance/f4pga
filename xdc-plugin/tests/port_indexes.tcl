848dd7b007088690afc37bd161761fc19ed2341d
Tomasz Michalak <tmichalak@antmicro.com>
2020-05-21 10:04:01 +0200

    XDC: Add verification step to tests
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- xdc-plugin/tests/port_indexes.tcl
+++ xdc-plugin/tests/port_indexes.tcl
@@ -10,16 +10,24 @@ synth_xilinx -vpr -flatten -abc9 -nosrl -noclkbuf -nodsp
 if {[info procs unknown] != ""} {
 	rename unknown ""
 }
-proc unknown args {puts "'unknown' proc command handler"}
-if {[catch {invalid command}]} {
+proc unknown args {return "'unknown' proc command handler"}
+set fp [open "port_indexes.txt" "w"]
+if {[catch {invalid command} result]} {
+	close $fp
 	error "Command should be handled by the 'unknown' proc"
+} else {
+	puts $fp $result
 }
 #Read the design constraints
 read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
 
-if {[catch {invalid command}]} {
+if {[catch {invalid command} result]} {
+	close $fp
 	error "Command should be handled by the 'unknown' proc"
+} else {
+	puts $fp $result
 }
+close $fp
 
 # Write the design in JSON format.
 write_json $::env(OUT_JSON)
