a8629ee95fe79c70a1ff1f45f38d8896e21bafaf
Tomasz Michalak <tmichalak@antmicro.com>
2020-07-17 15:17:27 +0200

    SDC: Add PLL example
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

---
+++ sdc-plugin/tests/pll/pll.tcl
@@ -0,0 +1,36 @@
+yosys -import
+plugin -i xdc
+plugin -i sdc
+# Import the commands from the plugins to the tcl interpreter
+yosys -import
+
+read_verilog pll.v
+read_verilog -specify -lib -D_EXPLICIT_CARRY +/xilinx/cells_sim.v
+read_verilog -lib +/xilinx/cells_xtra.v
+hierarchy -check -auto-top
+
+# Start flow after library reading
+synth_xilinx -vpr -flatten -abc9 -nosrl -nodsp -iopad -run prepare:check
+#
+##Read the design timing constraints
+#read_sdc $::env(INPUT_SDC_FILE)
+#return
+#
+##Read the design constraints
+#read_xdc -part_json $::env(PART_JSON) $::env(INPUT_XDC_FILE)
+#
+## Map Xilinx tech library to 7-series VPR tech library.
+#read_verilog -lib ../techmaps/cells_sim.v
+#techmap -map  ../techmaps/cells_map.v
+#
+## opt_expr -undriven makes sure all nets are driven, if only by the $undef
+## net.
+opt_expr -undriven
+opt_clean
+#
+setundef -zero -params
+stat
+#
+## Write the design in JSON format.
+write_json $::env(OUT_JSON)
+write_blif -attr -param -cname -conn $::env(OUT_EBLIF)
---
+++ sdc-plugin/tests/pll/pll.xdc
@@ -0,0 +1,9 @@
+# ## clk100:0
+set_property LOC E3 [get_ports clk100]
+set_property IOSTANDARD LVCMOS33 [get_ports clk100]
+# ## cpu_reset:0
+set_property LOC C2 [get_ports cpu_reset]
+set_property IOSTANDARD LVCMOS33 [get_ports cpu_reset]
+set_property LOC H5 [get_ports {led[0]}]
+set_property LOC J5 [get_ports {led[1]}]
+set_property LOC T9 [get_ports {led[2]}]
