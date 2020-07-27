b309d8241cc78fe99d8ca559d2c6463fc072c27e
Tomasz Michalak <tmichalak@antmicro.com>
2020-07-27 15:48:18 +0200

    SDC: Add create_clock command
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- sdc-plugin/sdc.cc
+++ sdc-plugin/sdc.cc
@@ -24,15 +24,8 @@ USING_YOSYS_NAMESPACE
 
 PRIVATE_NAMESPACE_BEGIN
 
-
-void register_in_tcl_interpreter(const std::string& command) {
-	Tcl_Interp* interp = yosys_get_tcl_interp();
-	std::string tcl_script = stringf("proc %s args { return [yosys %s {*}$args] }", command.c_str(), command.c_str());
-	Tcl_Eval(interp, tcl_script.c_str());
-}
-
-struct ReadSdc : public Frontend {
-	ReadSdc()
+struct ReadSdcCmd : public Frontend {
+	ReadSdcCmd()
 	       	: Frontend("sdc", "Read SDC file"){}
 
 	void help() override {
@@ -56,10 +49,98 @@ struct ReadSdc : public Frontend {
                 if (Tcl_EvalFile(interp, args[argidx].c_str()) != TCL_OK) {
                         log_cmd_error("TCL interpreter returned an error: %s\n", Tcl_GetStringResult(interp));
 		}
+	}
+};
 
+using Clocks = std::vector<RTLIL::Wire*>;
+
+struct CreateClockCmd : public Pass {
+	CreateClockCmd(Clocks& clocks)
+		: Pass("create_clock", "Create clock object")
+       		, clocks_(clocks)
+		{}
+
+	void help() override
+	{
+		log("\n");
+		log("    create_clock [ -name clock_name ] -period period_value [-waveform <edge_list>] <target>\n");
+		log("Define a clock.\n");
+	        log("If name is not specified then the name of the first target is selected as the clock's name.\n");
+		log("Period is expressed in nanoseconds.\n");
+		log("The waveform option specifies the duty cycle (the rising a falling edges) of the clock.\n");
+		log("It is specified as a list of two elements/time values: the first rising edge and the next falling edge.\n");
+		log("\n");
 	}
 
-} ReadSdc;
+        void execute(std::vector<std::string> args, RTLIL::Design *design) override
+        {
+                size_t argidx;
+		std::string name;
+		float rising_edge(0);
+		float falling_edge(0);
+		float period(0);
+                for (argidx = 1; argidx < args.size(); argidx++)
+                {
+                        std::string arg = args[argidx];
+                        if (arg == "-name" && argidx + 1 < args.size()) {
+                                name = args[++argidx];
+                                continue;
+                        }
+                        if (arg == "-period" && argidx + 1 < args.size()) {
+                                period = std::stof(args[++argidx]);
+                                continue;
+                        }
+                        if (arg == "-waveform" && argidx + 2 < args.size()) {
+                                rising_edge = std::stof(args[++argidx]);
+                                falling_edge = std::stof(args[++argidx]);
+                                continue;
+                        }
+			break;
+		}
+		// Add "w:" prefix to selection arguments to enforce wire object selection
+		AddWirePrefix(args, argidx);
+		extra_args(args, argidx, design);
+		// If clock name is not specified then take the name of the first target
+		for (auto module : design->modules()) {
+			if (!design->selected(module)) {
+				continue;
+			}
+			for (auto wire : module->wires()) {
+				if (design->selected(module, wire)) {
+					log("Selected wire %s\n", wire->name.c_str());
+					clocks_.push_back(wire);
+				}
+			}
+		}
+		if (name.empty()) {
+			name = clocks_.at(0)->name.str();
+		}
+
+		log("Created clock %s with period %f, waveform %f,%f\n", name.c_str(), period, rising_edge, falling_edge);
+	}
+
+	void AddWirePrefix(std::vector<std::string>& args, size_t argidx) {
+		auto selection_begin = args.begin() + argidx;
+		std::transform(selection_begin, args.end(), selection_begin, [](std::string& w) {return "w:" + w;});
+	}
+
+	Clocks& clocks_;
+};
+
+class SdcPlugin {
+	public:
+		SdcPlugin()
+		: create_clock_cmd_(clocks_)
+		{log("Loaded SDC plugin\n");}
+
+		ReadSdcCmd read_sdc_cmd_;
+		CreateClockCmd create_clock_cmd_;
+
+	private:
+		Clocks clocks_;
+} SdcPlugin;
+
+
 
 
 PRIVATE_NAMESPACE_END
