0819cafd3ff51fe95bbf05e5a1ff517f681e3410
Tomasz Michalak <tmichalak@antmicro.com>
2020-08-28 13:22:48 +0200

    Selection plugin: Add testing tcl procedure
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- selection-plugin/tests/counter/counter.tcl
+++ selection-plugin/tests/counter/counter.tcl
@@ -4,15 +4,37 @@ plugin -i selection
 # Import the commands from the plugins to the tcl interpreter
 yosys -import
 
+proc selection_to_tcl_list_through_file { expression } {
+    set file_name "[pid].txt"
+    select $expression -write $file_name
+    set fh [open $file_name r]
+    set result [list]
+    while {[gets $fh line] >= 0} {
+	lappend result $line
+    }
+    close $fh
+    file delete $file_name
+    return $result
+}
+
+proc test_selection { expression {debug 0} } {
+    if {$debug} {
+    	puts "List from file: [selection_to_tcl_list_through_file $expression]"
+    	puts "List in selection: [selection_to_tcl_list $expression]"
+    }
+    return [expr {[selection_to_tcl_list_through_file $expression] == [selection_to_tcl_list $expression]}]
+}
+
 read_verilog counter.v
 read_verilog -specify -lib -D_EXPLICIT_CARRY +/xilinx/cells_sim.v
 read_verilog -lib +/xilinx/cells_xtra.v
 hierarchy -check -auto-top
 
-puts "List: [selection_to_tcl_list w:*]"
+# Test the selection command and write results to file
+set rfh [open counter.txt w]
 
-# Write the selection to file
-set fh [open counter.txt w]
-set selection_list [selection_to_tcl_list w:*]
-puts $fh $selection_list
-close $fh
+puts $rfh [test_selection "t:*"]
+puts $rfh [test_selection "w:*"]
+puts $rfh [test_selection "*"]
+
+close $rfh
