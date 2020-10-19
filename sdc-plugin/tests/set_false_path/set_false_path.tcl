c7e908bdd14b20faa85fe122cbe103d0d4fefcfd
Tomasz Michalak <tmichalak@antmicro.com>
2020-10-19 15:31:05 +0200

    SDC: Add -through pin case for set_false_path command test
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- sdc-plugin/tests/set_false_path/set_false_path.tcl
+++ sdc-plugin/tests/set_false_path/set_false_path.tcl
@@ -16,4 +16,7 @@ set_false_path -quiet -from clk
 # -from clk to bottom_inst/I
 set_false_path -from clk -to bottom_inst.I
 
+# -through bottom_inst/I
+set_false_path -through bottom_inst.I
+
 write_sdc $::env(DESIGN_TOP).sdc
