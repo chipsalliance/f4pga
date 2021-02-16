48b95dde27c993cbc4f8ece906f19183e1355fd4
Tomasz Michalak <tmichalak@antmicro.com>
2021-02-16 10:43:07 +0100

    SDC: abc9: Fix test
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>

--- sdc-plugin/tests/abc9/abc9.tcl
+++ sdc-plugin/tests/abc9/abc9.tcl
@@ -5,8 +5,8 @@ yosys -import  ;# ingest plugin commands
 # ensure abc9.D is unset
 scratchpad -assert-unset abc9.D
 
-read_verilog abc9.v
-read_sdc abc9.sdc
+read_verilog $::env(DESIGN_TOP).v
+read_sdc $::env(DESIGN_TOP).input.sdc
 
 # check that abc9.D was set to half the fastest clock period in the design
 scratchpad -assert abc9.D 5000
