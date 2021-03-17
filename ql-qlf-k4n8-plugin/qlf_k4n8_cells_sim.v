d6b41b12d1ba00ca5b67a8a3619bac151b075db6
Lalit Sharma <lsharma@quicklogic.com>
2021-03-17 09:46:35 -0700

    Updating adder_lut4 INIT param as there is a change in qlf_k4n8 device adder_lut4 representation
    Signed-off-by: Lalit Sharma <lsharma@quicklogic.com>

--- ql-qlf-k4n8-plugin/qlf_k4n8_cells_sim.v
+++ ql-qlf-k4n8-plugin/qlf_k4n8_cells_sim.v
@@ -14,15 +14,15 @@ module adder_lut4(
 
     // Output function
     wire [0:7] s1 = li[0] ?
-        {LUT[1], LUT[3], LUT[5], LUT[7], LUT[9], LUT[11], LUT[13], LUT[15]}:
-        {LUT[0], LUT[2], LUT[4], LUT[6], LUT[8], LUT[10], LUT[12], LUT[14]};
+        {LUT[0], LUT[2], LUT[4], LUT[6], LUT[8], LUT[10], LUT[12], LUT[14]}:
+        {LUT[1], LUT[3], LUT[5], LUT[7], LUT[9], LUT[11], LUT[13], LUT[15]};
 
-    wire [0:3] s2 = li[1] ? {s1[1], s1[3], s1[5], s1[7]} :
-                            {s1[0], s1[2], s1[4], s1[6]};
+    wire [0:3] s2 = li[1] ? {s1[0], s1[2], s1[4], s1[6]} : 
+                            {s1[1], s1[3], s1[5], s1[7]};
 
-    wire [0:1] s3 = li[2] ? {s2[1], s2[3]} : {s2[0], s2[2]};
+    wire [0:1] s3 = li[2] ? {s2[0], s2[2]} : {s2[1], s2[3]};
 
-    assign lut4_out = li[3] ? s3[1] : s3[0];
+    assign lut4_out = li[3] ? s3[0] : s3[1];
     
     // Carry out function
     assign cout = (s2[2]) ? cin : s2[3];
@@ -41,18 +41,18 @@ module frac_lut4(
 
     // Output function
     wire [0:7] s1 = li[0] ?
-        {LUT[1], LUT[3], LUT[5], LUT[7], LUT[9], LUT[11], LUT[13], LUT[15]}:
-        {LUT[0], LUT[2], LUT[4], LUT[6], LUT[8], LUT[10], LUT[12], LUT[14]};
+        {LUT[0], LUT[2], LUT[4], LUT[6], LUT[8], LUT[10], LUT[12], LUT[14]}:
+        {LUT[1], LUT[3], LUT[5], LUT[7], LUT[9], LUT[11], LUT[13], LUT[15]};
 
-    wire [0:3] s2 = li[1] ? {s1[1], s1[3], s1[5], s1[7]} :
-                            {s1[0], s1[2], s1[4], s1[6]};
+    wire [0:3] s2 = li[1] ? {s1[0], s1[2], s1[4], s1[6]} : 
+                            {s1[1], s1[3], s1[5], s1[7]};
 
-    wire [0:1] s3 = li[2] ? {s2[1], s2[3]} : {s2[0], s2[2]};
+    wire [0:1] s3 = li[2] ? {s2[0], s2[2]} : {s2[1], s2[3]};
 
     assign lut2_out[0] = s2[2];
     assign lut2_out[1] = s2[3];
 
-    assign  lut4_out = li[3] ? s3[1] : s3[0];
+    assign  lut4_out = li[3] ? s3[0] : s3[1];
 
 endmodule
 
@@ -132,3 +132,4 @@ module dffs(
         else
             Q <= D;
 endmodule
+
