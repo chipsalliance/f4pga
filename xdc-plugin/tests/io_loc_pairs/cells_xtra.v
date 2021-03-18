f908ea9df294a096f0cd5324a4fadc4e53dec655
Alessandro Comodi <acomodi@antmicro.com>
2021-03-18 20:19:44 +0100

    xdc: add comment on GTPE2_CHANNEL IO_LOC_PAIR param
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ xdc-plugin/tests/io_loc_pairs/cells_xtra.v
@@ -0,0 +1,12 @@
+module GTPE2_CHANNEL (
+    (* iopad_external_pin *)
+    output GTPTXN,
+    (* iopad_external_pin *)
+    output GTPTXP,
+    (* iopad_external_pin *)
+    input GTPRXN,
+    (* iopad_external_pin *)
+    input GTPRXP
+);
+
+endmodule
