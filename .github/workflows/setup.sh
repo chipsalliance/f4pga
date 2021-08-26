82d39a92a19badbbf7a7f82c4ac3672e03941697
Alessandro Comodi <acomodi@antmicro.com>
2021-08-26 19:50:10 +0200

    ci: use yosys from conda
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -39,22 +39,14 @@ end_section
 #Install yosys
 start_section Install-Yosys
 (
-    if [ ! -e ~/.local-bin/bin/yosys ]; then
-        echo '=========================='
-        echo 'Building yosys'
-        echo '=========================='
-        mkdir -p ~/.local-src
-        mkdir -p ~/.local-bin
-        cd ~/.local-src
-        git clone https://github.com/YosysHQ/yosys.git
-        cd yosys
-        make config-gcc # Build Yosys using GCC
-        PREFIX=$HOME/.local-bin make -j$(nproc)
-        PREFIX=$HOME/.local-bin make install
-        echo $(which yosys)
-        echo $(which yosys-config)
-        echo $(yosys-config --datdir)
-    fi
+    echo '=========================='
+    echo 'Making env with yosys'
+    echo '=========================='
+    make env
+    make enter
+    echo $(which yosys)
+    echo $(which yosys-config)
+    echo $(yosys-config --datdir)
 )
 end_section
 
