c607966b365a75f25e8f1eda976c3ff2cf02aec1
Alessandro Comodi <acomodi@antmicro.com>
2021-08-26 10:55:47 +0200

    tests: add clean processes step before the write_json step
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

--- sdc-plugin/tests/restore_from_json/restore_from_json.tcl
+++ sdc-plugin/tests/restore_from_json/restore_from_json.tcl
@@ -6,6 +6,10 @@ read_verilog $::env(DESIGN_TOP).v
 synth_xilinx
 create_clock -period 10 clk
 propagate_clocks
+
+# Clean processes before writing JSON.
+yosys proc
+
 write_sdc [test_output_path "restore_from_json_1.sdc"]
 write_json [test_output_path "restore_from_json.json"]
 
