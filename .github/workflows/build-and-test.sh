74f2765ecec764d434a1ec6cf462c8dc0b6f9233
Kamil Rakoczy <krakoczy@antmicro.com>
2021-09-10 09:36:16 +0200

    CI: disable QL tests and enable other tests
    Signed-off-by: Kamil Rakoczy <krakoczy@antmicro.com>

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -25,11 +25,9 @@ end_section
 
 ##########################################################################
 
-#Disable testing, as for now there isn't any uhdm-plugin related tests
-#and some other test is failing
-#start_section Testing
-#make test -j`nproc`
-#end_section
+start_section Testing
+make test -j`nproc`
+end_section
 
 ##########################################################################
 
