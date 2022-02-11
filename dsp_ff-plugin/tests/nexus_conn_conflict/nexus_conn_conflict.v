8e1a3f30e6677d4d4a04be75cb949a0f10efae4d
Maciej Kurc <mkurc@antmicro.com>
2022-02-11 16:04:55 +0100

    Added license headers
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- dsp_ff-plugin/tests/nexus_conn_conflict/nexus_conn_conflict.v
+++ dsp_ff-plugin/tests/nexus_conn_conflict/nexus_conn_conflict.v
@@ -1,3 +1,11 @@
+// Copyright (C) 2020-2021  The SymbiFlow Authors.
+//
+// Use of this source code is governed by a ISC-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/ISC
+//
+// SPDX-License-Identifier:ISC
+
 module conflict_dsp_clk (
     input  wire        CLK_A,
     input  wire        CLK_B,
