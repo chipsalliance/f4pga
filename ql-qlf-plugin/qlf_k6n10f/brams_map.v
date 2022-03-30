53a7f655dc47a5fef86ac48eeea1e5dfa614fbf3
Paweł Czarnecki <pczarnecki@antmicro.com>
2022-03-30 16:23:08 +0200

    ql-qlf: qlf_k6n10f: bram: update TDP_BRAM18 techmap
    Signed-off-by: Paweł Czarnecki <pczarnecki@antmicro.com>

--- ql-qlf-plugin/qlf_k6n10f/brams_map.v
+++ ql-qlf-plugin/qlf_k6n10f/brams_map.v
@@ -181,17 +181,20 @@ endmodule
 
 // ------------------------------------------------------------------------
 
-module \$__QLF_FACTOR_BRAM18_TDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DATA, B1EN);
-	parameter CFG_ABITS = 10;
+module \$__QLF_FACTOR_BRAM18_TDP (A1ADDR, A1DATA, A1EN, B1ADDR, B1DATA, B1EN, C1ADDR, C1DATA, C1EN, CLK1, CLK2, CLK3, CLK4, D1ADDR, D1DATA, D1EN);
+	parameter CFG_ABITS = 11;
 	parameter CFG_DBITS = 18;
-	parameter CFG_ENABLE_B = 2;
+	parameter CFG_ENABLE_B = 4;
+	parameter CFG_ENABLE_D = 4;
 
 	parameter CLKPOL2 = 1;
 	parameter CLKPOL3 = 1;
 	parameter [18431:0] INIT = 18432'bx;
 
+	input CLK1;
 	input CLK2;
 	input CLK3;
+	input CLK4;
 
 	input [CFG_ABITS-1:0] A1ADDR;
 	output [CFG_DBITS-1:0] A1DATA;
@@ -201,101 +204,55 @@ module \$__QLF_FACTOR_BRAM18_TDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DA
 	input [CFG_DBITS-1:0] B1DATA;
 	input [CFG_ENABLE_B-1:0] B1EN;
 
-	wire [13:0] A1ADDR_14;
-	wire [13:0] B1ADDR_14;
-	//wire [3:0] B1EN_4 = B1EN;
+	input [CFG_ABITS-1:0] C1ADDR;
+	output [CFG_DBITS-1:0] C1DATA;
+	input C1EN;
 
-	wire [1:0] DIP, DOP;
-	wire [15:0] DI, DO;
+	input [CFG_ABITS-1:0] D1ADDR;
+	input [CFG_DBITS-1:0] D1DATA;
+	input [CFG_ENABLE_D-1:0] D1EN;
 
-	wire [15:0] DOBDO;
-	wire [1:0] DOPBDOP;
+	BRAM2x18_TDP #(
+		.CFG_ABITS(CFG_ABITS),
+		.CFG_DBITS(CFG_DBITS),
+		.CFG_ENABLE_B(CFG_ENABLE_B),
+		.CFG_ENABLE_D(CFG_ENABLE_D),
+		.CLKPOL2(CLKPOL2),
+		.CLKPOL3(CLKPOL3),
+		.INIT0(INIT),
+	) _TECHMAP_REPLACE_ (
+		.A1ADDR(A1ADDR),
+		.A1DATA(A1DATA),
+		.A1EN(A1EN),
+		.B1ADDR(B1ADDR),
+		.B1DATA(B1DATA),
+		.B1EN(B1EN),
+		.CLK1(CLK1),
 
-	assign A1DATA = { DOP[1], DO[15: 8], DOP[0], DO[ 7: 0] };
-	assign { DIP[1], DI[15: 8], DIP[0], DI[ 7: 0] } = B1DATA;
-        
-        assign A1ADDR_14[13:CFG_ABITS]  = 0;
-        assign A1ADDR_14[CFG_ABITS-1:0] = A1ADDR;
-        assign B1ADDR_14[13:CFG_ABITS]  = 0;
-        assign B1ADDR_14[CFG_ABITS-1:0] = B1ADDR;
+		.C1ADDR(C1ADDR),
+		.C1DATA(C1DATA),
+		.C1EN(C1EN),
+		.D1ADDR(D1ADDR),
+		.D1DATA(D1DATA),
+		.D1EN(D1EN),
+		.CLK2(CLK2),
 
-	/*if (CFG_DBITS == 1) begin
-	  assign WRITEDATAWIDTHB = 3'b000;
-	  assign READDATAWIDTHA = 3'b000;
-	end else if (CFG_DBITS == 2) begin
-          assign WRITEDATAWIDTHB = 3'b001;
-          assign READDATAWIDTHA = 3'b001;
-        end else if (CFG_DBITS > 2 && CFG_DBITS <= 4) begin
-          assign WRITEDATAWIDTHB = 3'b010;
-          assign READDATAWIDTHA = 3'b010;
-        end else if (CFG_DBITS > 4 && CFG_DBITS <= 9) begin
-          assign WRITEDATAWIDTHB = 3'b011;
-          assign READDATAWIDTHA = 3'b011;
-        end else if (CFG_DBITS > 9 && CFG_DBITS <= 18) begin
-          //assign WRITEDATAWIDTHB = 3'b100;
-          assign READDATAWIDTHA = 3'b100;
-	end*/
-	generate if (CFG_DBITS > 8) begin
-		TDP_BRAM18 #(
-			//`include "brams_init_18.vh"
-                        .READ_WIDTH_A(CFG_DBITS),
-                        .READ_WIDTH_B(CFG_DBITS),
-                        .WRITE_WIDTH_A(CFG_DBITS),
-                        .WRITE_WIDTH_B(CFG_DBITS),
-		) _TECHMAP_REPLACE_ (
-			.WRITEDATAA(16'hFFFF),
-			.WRITEDATAAP(2'b11),
-			.READDATAA(DO[15:0]),
-			.READDATAAP(DOP[2:0]),
-			.ADDRA(A1ADDR_14),
-			.CLOCKA(CLK2),
-			.READENABLEA(A1EN),
-			.WRITEENABLEA(1'b0),
-			.BYTEENABLEA(2'b0),
-			//.WRITEDATAWIDTHA(3'b0),
-			//.READDATAWIDTHA(READDATAWIDTHA),
+		.E1ADDR(),
+		.E1DATA(),
+		.E1EN(),
+		.F1ADDR(),
+		.F1DATA(),
+		.F1EN(),
+		.CLK3(),
 
-			.WRITEDATAB(DI),
-			.WRITEDATABP(DIP),
-			.READDATAB(DOBDO),
-			.READDATABP(DOPBDOP),
-			.ADDRB(B1ADDR_14),
-			.CLOCKB(CLK3),
-			.READENABLEB(1'b0),
-			.WRITEENABLEB(1'b1),
-			.BYTEENABLEB(B1EN)
-			//.WRITEDATAWIDTHB(WRITEDATAWIDTHB),
-			//.READDATAWIDTHB(3'b0)
-		);
-	end else begin
-		TDP_BRAM18 #(
-			//`include "brams_init_16.vh"
-		) _TECHMAP_REPLACE_ (
-			.WRITEDATAA(16'hFFFF),
-			.WRITEDATAAP(2'b11),
-			.READDATAA(DO[15:0]),
-			.READDATAAP(DOP[2:0]),
-			.ADDRA(A1ADDR_14),
-			.CLOCKA(CLK2),
-			.READENABLEA(A1EN),
-			.WRITEENABLEA(1'b0),
-			.BYTEENABLEA(2'b0),
-			//.WRITEDATAWIDTHA(3'b0),
-		//	.READDATAWIDTHA(READDATAWIDTHA),
-
-			.WRITEDATAB(DI),
-			.WRITEDATABP(DIP),
-			.READDATAB(DOBDO),
-			.READDATABP(DOPBDOP),
-			.ADDRB(B1ADDR_14),
-			.CLOCKB(CLK3),
-			.READENABLEB(1'b0),
-			.WRITEENABLEB(1'b1),
-			.BYTEENABLEB(B1EN)
-			//.WRITEDATAWIDTHB(WRITEDATAWIDTHB),
-			//.READDATAWIDTHB(3'b0)
-		);
-	end endgenerate
+		.G1ADDR(),
+		.G1DATA(),
+		.G1EN(),
+		.H1ADDR(),
+		.H1DATA(),
+		.H1EN(),
+		.CLK4()
+	);
 endmodule
 
 module \$__QLF_FACTOR_BRAM36_SDP (CLK2, CLK3, A1ADDR, A1DATA, A1EN, B1ADDR, B1DATA, B1EN);
