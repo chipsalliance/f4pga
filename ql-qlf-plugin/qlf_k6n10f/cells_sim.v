dce1529588347b9bfe4f7382e166dd413ca13ed9
Maciej Kurc <mkurc@antmicro.com>
2022-04-13 09:45:53 +0200

    Fixed incorrect acc_fir_i port width.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- ql-qlf-plugin/qlf_k6n10f/cells_sim.v
+++ ql-qlf-plugin/qlf_k6n10f/cells_sim.v
@@ -1651,7 +1651,7 @@ endmodule
 module dsp_t1_20x18x64 (
     input  [19:0] a_i,
     input  [17:0] b_i,
-    input  [ 3:0] acc_fir_i,
+    input  [ 5:0] acc_fir_i,
     output [37:0] z_o,
     output [17:0] dly_b_o,
 
@@ -1712,7 +1712,7 @@ endmodule
 module dsp_t1_10x9x32 (
     input  [ 9:0] a_i,
     input  [ 8:0] b_i,
-    input  [ 1:0] acc_fir_i,
+    input  [ 5:0] acc_fir_i,
     output [18:0] z_o,
     output [ 8:0] dly_b_o,
 
@@ -1754,7 +1754,7 @@ module dsp_t1_10x9x32 (
 
     .f_mode(1'b1),  // 10x9x32 DSP
 
-    .acc_fir({2'd0, acc_fir_i}),
+    .acc_fir(acc_fir_i),
     .feedback(feedback_i),
     .load_acc(load_acc_i),
 
