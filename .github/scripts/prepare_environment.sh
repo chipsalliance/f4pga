4b8594d195b0f2173a377bbbdb3d4c2d1f5c3e9f
Pawel Czarnecki <pczarnecki@antmicro.com>
2022-07-28 14:22:28 +0200

    CI: ql: prepare infrastructure for updated arch-defs packages
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- .github/scripts/prepare_environment.sh
+++ .github/scripts/prepare_environment.sh
@@ -48,17 +48,21 @@ echo '::endgroup::'
 
 
 echo '::group::Install arch-defs'
+mkdir -p "$F4PGA_INSTALL_DIR_FAM"/install
 case "$FPGA_FAM" in
   xc7)
-    mkdir -p "$F4PGA_INSTALL_DIR_FAM"/install
     F4PGA_TIMESTAMP='20220714-173445'
     F4PGA_HASH='f7afc12'
     for PKG in install xc7a50t_test; do
-      wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/${F4PGA_TIMESTAMP}/symbiflow-arch-defs-${PKG}-${F4PGA_HASH}.tar.xz | tar -xJC $F4PGA_INSTALL_DIR/xc7/install
+      wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/${F4PGA_TIMESTAMP}/symbiflow-arch-defs-${PKG}-${F4PGA_HASH}.tar.xz | tar -xJC $F4PGA_INSTALL_DIR/$FPGA_FAM/install
     done
   ;;
   eos-s3)
-    wget -qO- https://storage.googleapis.com/symbiflow-arch-defs-install/quicklogic-arch-defs-qlf-fc5d8da.tar.gz | tar -xz -C $F4PGA_INSTALL_DIR_FAM
+    F4PGA_TIMESTAMP='yyyymmdd-hhmmss'   #FIXME: placeholder timestamp
+    F4PGA_HASH='deadbee'                #FIXME: placeholder hash
+    for PKG in install-ql ql-eos-s3_wlcsp; do
+      wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/${F4PGA_TIMESTAMP}/symbiflow-arch-defs-${PKG}-${F4PGA_HASH}.tar.xz | tar -xJC $F4PGA_INSTALL_DIR/$FPGA_FAM/install
+    done
   ;;
 esac
 echo '::endgroup::'
