b7303057011197f7007c4660bc3b2a2510db61fe
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-02 02:20:55 +0200

    f4pga/setup: optionally use .gitcommit
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- f4pga/setup.py
+++ f4pga/setup.py
@@ -55,6 +55,11 @@ def get_requirements(file: Path) -> List[str]:
 semver = "0.0.0"
 version = None
 
+with (packagePath.parent / '.gitcommit').open("r") as rptr:
+    sha = rptr.read().strip()
+    if sha != '$Format:%h$':
+        version = f'{semver}-{sha}'
+
 git = which('git')
 if git is not None:
     proc = run(['git', 'rev-parse', 'HEAD'], capture_output=True)
