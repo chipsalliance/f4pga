1688a9776330cc4975b13fc037617c35fb466cda
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-04 04:02:29 +0200

    f4pga: the default share path does not include subdir 'install' by default
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- .github/scripts/prepare_environment.sh
+++ .github/scripts/prepare_environment.sh
@@ -38,15 +38,15 @@ echo '::group::Install Miniconda3'
 
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O conda_installer.sh
 
-bash conda_installer.sh -u -b -p "${F4PGA_INSTALL_DIR}/${FPGA_FAM}"/conda
-source "${F4PGA_INSTALL_DIR}/${FPGA_FAM}"/conda/etc/profile.d/conda.sh
+bash conda_installer.sh -u -b -p "${F4PGA_INSTALL_DIR_FAM}"/conda
+source "${F4PGA_INSTALL_DIR_FAM}"/conda/etc/profile.d/conda.sh
 
 echo '::endgroup::'
 
 
 echo '::group::Install arch-defs'
 
-mkdir -p "$F4PGA_INSTALL_DIR_FAM_ROOT"
+mkdir -p "$F4PGA_INSTALL_DIR_FAM"
 
 F4PGA_TIMESTAMP='20220803-160711'
 F4PGA_HASH='df6d9e5'
@@ -61,7 +61,7 @@ esac
 
 for PKG in $PACKAGES; do
   wget -qO- https://storage.googleapis.com/symbiflow-arch-defs/artifacts/prod/foss-fpga-tools/symbiflow-arch-defs/continuous/install/${F4PGA_TIMESTAMP}/symbiflow-arch-defs-${PKG}-${F4PGA_HASH}.tar.xz \
-    | tar -xJC $F4PGA_INSTALL_DIR_FAM_ROOT
+    | tar -xJC $F4PGA_INSTALL_DIR_FAM
 done
 
 echo '::endgroup::'
@@ -69,6 +69,6 @@ echo '::endgroup::'
 
 echo '::group::Create environment'
 
-conda env create -f $F4PGA_INSTALL_DIR_FAM_ROOT/"$FPGA_FAM"_env/"$FPGA_FAM"_environment.yml
+conda env create -f $F4PGA_INSTALL_DIR_FAM/"$FPGA_FAM"_env/"$FPGA_FAM"_environment.yml
 
 echo '::endgroup::'
--- f4pga/__init__.py
+++ f4pga/__init__.py
@@ -80,7 +80,7 @@ bin_dir_path = \
     environ.get('F4PGA_BIN_DIR', str(Path(sys_argv[0]).resolve().parent.parent))
 share_dir_path = \
     environ.get('F4PGA_SHARE_DIR',
-                str(Path(f'{install_dir}/{FPGA_FAM}/install/share/f4pga').resolve()))
+                str(Path(f'{install_dir}/{FPGA_FAM}/share/f4pga').resolve()))
 
 class DependencyNotProducedException(F4PGAException):
     dep_name: str
