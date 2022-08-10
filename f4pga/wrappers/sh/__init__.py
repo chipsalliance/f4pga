8720c4ff25bdeecf8a010271ba00339f7cdcc494
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-10 21:27:27 +0200

    f4pga/wrappers/sh/quicklogic: rm generate_libfile.f4pga.sh
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- f4pga/wrappers/sh/__init__.py
+++ f4pga/wrappers/sh/__init__.py
@@ -166,7 +166,32 @@ def generate_bitstream():
 
 def generate_libfile():
     print("[F4PGA] Running (deprecated) generate_libfile")
-    run_sh_script(ROOT / "quicklogic/generate_libfile.f4pga.sh")
+    run_bash_cmds(f"""
+set -e
+PART=$1
+DEVICE=$2
+CORNER=$3
+
+if [[ "$DEVICE" =~ ^(qlf_k4n8_qlf_k4n8)$ ]];then
+  DEVICE_1="qlf_k4n8-qlf_k4n8_umc22_$CORNER"
+  PINMAPXML="pinmap_qlf_k4n8_umc22.xml"
+  INTERFACEXML="interface-mapping_24x24.xml"
+  DEV="qlf_k4n8_umc22"
+else
+  DEVICE_1=${{DEVICE}}
+fi
+
+ARCH_DIR="$F4PGA_SHARE_DIR"/arch/${{DEVICE_1}}_${{DEVICE_1}}
+PINMAP_XML=${{ARCH_DIR}}/${{PINMAPXML}}
+
+'{which('python3')}' "$F4PGA_SHARE_DIR"/scripts/create_lib.py \
+  -n "${{DEV}}_0P72_SSM40" \
+  -m fpga_top \
+  -c "${{PART}}" \
+  -x "${{ARCH_DIR}}/lib/${{INTERFACEXML}}" \
+  -l "${{DEV}}_0P72_SSM40.lib" \
+  -t "${{ARCH_DIR}}/lib"
+""")
 
 
 def ql():
