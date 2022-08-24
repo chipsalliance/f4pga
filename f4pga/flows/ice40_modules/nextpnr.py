7fb473b1d7d67bab748576cb04dea859e73e14f5
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-24 21:15:16 +0100

    f4pga: run black
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- f4pga/flows/ice40_modules/nextpnr.py
+++ f4pga/flows/ice40_modules/nextpnr.py
@@ -24,57 +24,53 @@ from f4pga.flows.tools.nextpnr import NextPnrBaseModule
 import re
 from pathlib import Path
 
+
 class Ice40ChipInfo:
     subfamily: str
     size: str
     package_code: str
 
     def __init__(self, part_name: str):
-        m = re.match('ICE40([A-Z]*)([0-9]+[A-Z]?)-([A-Z0-9]*)$', part_name.upper())
+        m = re.match("ICE40([A-Z]*)([0-9]+[A-Z]?)-([A-Z0-9]*)$", part_name.upper())
         assert m is not None
 
         self.subfamily = m.group(1)
         self.size = m.group(2)
         self.package_code = m.group(3)
 
+
 class NextPnrModule(NextPnrBaseModule):
-    def map_io(self, ctx: ModuleContext) -> 'dict[str, ]':
-        return {
-            'ice_asm': str(Path(ctx.takes.json).with_suffix('.ice'))
-        }
+    def map_io(self, ctx: ModuleContext) -> "dict[str, ]":
+        return {"ice_asm": str(Path(ctx.takes.json).with_suffix(".ice"))}
 
     def execute(self, ctx: ModuleContext):
         chip_info = Ice40ChipInfo(ctx.values.part_name)
 
         self.extra_nextpnr_opts = [
-            f'--{(chip_info.subfamily + chip_info.size).lower()}',
-            f'--package', chip_info.package_code.lower(),
-            f'--asc', ctx.outputs.ice_asm
+            f"--{(chip_info.subfamily + chip_info.size).lower()}",
+            f"--package",
+            chip_info.package_code.lower(),
+            f"--asc",
+            ctx.outputs.ice_asm,
         ]
 
         if ctx.takes.pcf is not None:
-            self.extra_nextpnr_opts += ['--pcf', ctx.takes.pcf]
+            self.extra_nextpnr_opts += ["--pcf", ctx.takes.pcf]
         else:
-            self.extra_nextpnr_opts += ['--pcf-allow-unconstrained']
+            self.extra_nextpnr_opts += ["--pcf-allow-unconstrained"]
 
         return super().execute(ctx)
 
-    def __init__(self, params: 'dict[str, ]', r_env: ResolutionEnv,
-                 instance_name: str = '<anonymous>'):
+    def __init__(self, params: "dict[str, ]", r_env: ResolutionEnv, instance_name: str = "<anonymous>"):
         super().__init__(params, r_env, instance_name, interchange=False)
 
-        self.nextpnr_variant = 'ice40'
+        self.nextpnr_variant = "ice40"
 
-        self.takes += [
-            'pcf?'
-        ]
+        self.takes += ["pcf?"]
 
-        self.values += [
-            'part_name'
-        ]
+        self.values += ["part_name"]
 
-        self.produces += [
-            'ice_asm'
-        ]
+        self.produces += ["ice_asm"]
+
 
 ModuleClass = NextPnrModule
--- f4pga/flows/tools/nextpnr.py
+++ f4pga/flows/tools/nextpnr.py
@@ -23,66 +23,66 @@ from f4pga.flows.common import *
 
 class NextPnrBaseModule(Module):
     nextpnr_variant: str
-    extra_nextpnr_opts: 'list[str]'
-    nextpnr_log_name: 'str | None'
+    extra_nextpnr_opts: "list[str]"
+    nextpnr_log_name: "str | None"
     use_interchange: bool
 
-    def map_io(self, ctx: ModuleContext) -> 'dict[str, ]':
+    def map_io(self, ctx: ModuleContext) -> "dict[str, ]":
         return {}
 
     def execute(self, ctx: ModuleContext):
-        nextpnr_cmd = f'nextpnr-{self.nextpnr_variant}'
+        nextpnr_cmd = f"nextpnr-{self.nextpnr_variant}"
 
         nextpnr_opts = [
-            '--top', ctx.values.top,
-            '--placer', ctx.values.placer,
-            '--router', ctx.values.router,
+            "--top",
+            ctx.values.top,
+            "--placer",
+            ctx.values.placer,
+            "--router",
+            ctx.values.router,
         ]
 
         if self.use_interchange:
-            nextpnr_opts += ['--netlist', ctx.takes.ic_logical_netlist]
+            nextpnr_opts += ["--netlist", ctx.takes.ic_logical_netlist]
         else:
-            nextpnr_opts += ['--json', ctx.takes.json]
+            nextpnr_opts += ["--json", ctx.takes.json]
 
         if ctx.values.prepack_script is not None:
-            nextpnr_opts += ['--pre-pack', ctx.values.prepack_script]
+            nextpnr_opts += ["--pre-pack", ctx.values.prepack_script]
         if ctx.values.preplace_script is not None:
-            nextpnr_opts += ['--pre-place', ctx.values.preplace_script]
+            nextpnr_opts += ["--pre-place", ctx.values.preplace_script]
         if ctx.values.preroute_script is not None:
-            nextpnr_opts += ['--pre-route', ctx.values.preroute_script]
+            nextpnr_opts += ["--pre-route", ctx.values.preroute_script]
         if ctx.values.postroute_script is not None:
-            nextpnr_opts += ['--post-poute', ctx.values.postroute_script]
+            nextpnr_opts += ["--post-poute", ctx.values.postroute_script]
         if ctx.values.fail_script is not None:
-            nextpnr_opts += ['--on-fail', ctx.values.fail_script]
+            nextpnr_opts += ["--on-fail", ctx.values.fail_script]
 
         if ctx.values.thread_count:
-            nextpnr_opts += ['--threads', ctx.values.thread_count]
+            nextpnr_opts += ["--threads", ctx.values.thread_count]
         if ctx.values.parallel:
-            nextpnr_opts += ['--parallel-refine']
+            nextpnr_opts += ["--parallel-refine"]
 
         nextpnr_opts += self.extra_nextpnr_opts
 
-
         if get_verbosity_level() >= 2:
-            yield  'Place-and-routing with nextpnr...\n ' \
-                  f'{nextpnr_cmd} {" ".join(nextpnr_opts)}'
+            yield "Place-and-routing with nextpnr...\n " f'{nextpnr_cmd} {" ".join(nextpnr_opts)}'
         else:
-            yield 'Place-and-routing with nextpnr...'
-
+            yield "Place-and-routing with nextpnr..."
 
         res = sub(nextpnr_cmd, *nextpnr_opts)
 
-        yield 'Saving log...'
+        yield "Saving log..."
         log_path = getattr(ctx.outputs, self.nextpnr_log_name)
         if log_path is not None:
-            with open(log_path, 'w') as f:
+            with open(log_path, "w") as f:
                 f.write(res.decode())
 
-
-    def __init__(self, params: 'dict[str, ]', r_env: ResolutionEnv,
-                 instance_name: str = '<anonymous>', interchange=False):
+    def __init__(
+        self, params: "dict[str, ]", r_env: ResolutionEnv, instance_name: str = "<anonymous>", interchange=False
+    ):
         super().__init__(params, r_env, instance_name)
-        self.nextpnr_variant = 'unknown'
+        self.nextpnr_variant = "unknown"
         self.extra_nextpnr_opts = []
 
         self.no_of_phases = 2
@@ -90,28 +90,26 @@ class NextPnrBaseModule(Module):
 
         if self.use_interchange:
             self.takes = [
-                'ic_logical_netlist',
+                "ic_logical_netlist",
             ]
         else:
             self.takes = [
-                'json',
+                "json",
             ]
 
         self.values = [
-            'top',
-            'placer',
-            'router',
-            'prepack_script?',
-            'preplace_script?',
-            'preroute_script?',
-            'postroute_script?',
-            'fail_script?',
-            'thread_count?',
-            'parallel?'
+            "top",
+            "placer",
+            "router",
+            "prepack_script?",
+            "preplace_script?",
+            "preroute_script?",
+            "postroute_script?",
+            "fail_script?",
+            "thread_count?",
+            "parallel?",
         ]
 
-        self.nextpnr_log_name = f'nextpnr_{instance_name}_log'
+        self.nextpnr_log_name = f"nextpnr_{instance_name}_log"
 
-        self.produces = [
-            f'{self.nextpnr_log_name}!'
-        ]
+        self.produces = [f"{self.nextpnr_log_name}!"]
