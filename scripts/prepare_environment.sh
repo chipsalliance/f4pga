cf35028b0a0fe72f8fa55219b8f20f983dd59829
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-08-29 21:27:25 +0200

    scripts/prepare_environment: remove all python scripts from arch-defs packages
    Signed-off-by: Unai Martinez-Corral <umartinezcorral@antmicro.com>

--- scripts/prepare_environment.sh
+++ scripts/prepare_environment.sh
@@ -70,10 +70,7 @@ for PKG in $PACKAGES; do
     | tar -xJC $F4PGA_INSTALL_DIR_FAM
 done
 
-case "$FPGA_FAM" in
-  xc7) rm -vrf $F4PGA_INSTALL_DIR_FAM/share/f4pga/scripts ;;
-  eos-s3) find $F4PGA_INSTALL_DIR_FAM/share -name split_inouts.py | xargs rm -v ;;
-esac
+rm -vrf $F4PGA_INSTALL_DIR_FAM/share/f4pga/scripts
 
 echo '::endgroup::'
 
