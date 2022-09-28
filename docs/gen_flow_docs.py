962c2164b6fec1b68326b6aaaacc29546b39e004
Krzysztof Boronski <kboronski@antmicro.com>
2022-09-28 21:46:05 +0200

    Add license header to gen_flow_docs.py
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

--- docs/gen_flow_docs.py
+++ docs/gen_flow_docs.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 F4PGA Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # XXX: Hack to make f4pga think that we are in an environment set-up for eos-s3.
 # eos-s3 has been chosen because it does not require prjxray-config which is used when
 # configuring variables available for xc7.
